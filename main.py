e01ef29f14b6f1828088d3690ccbe45d0bb5a74b
lilinyang <lilinyang@users.noreply.github.com>
2024-08-01 17:45:08 +0800

    上传项目前后端代码文件
    上传项目前后端代码文件

---
+++ main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from langchain.memory import ConversationBufferMemory
+
+from utils import get_chat_response
+
+st.title("💬 克隆ChatGPT")
+
+with st.sidebar:
+    openai_api_key = st.text_input("请输入OpenAI API Key：", type="password")
+    st.markdown("[获取OpenAI API key](https://platform.openai.com/account/api-keys)")
+
+if "memory" not in st.session_state:
+    st.session_state["memory"] = ConversationBufferMemory(return_messages=True)
+    st.session_state["messages"] = [{"role": "ai",
+                                     "content": "你好，我是你的AI助手，有什么可以帮你的吗？"}]
+
+for message in st.session_state["messages"]:
+    st.chat_message(message["role"]).write(message["content"])
+
+prompt = st.chat_input()
+if prompt:
+    if not openai_api_key:
+        st.info("请输入你的OpenAI API Key")
+        st.stop()
+    st.session_state["messages"].append({"role": "human", "content": prompt})
+    st.chat_message("human").write(prompt)
+
+    with st.spinner("AI正在思考中，请稍等..."):
+        response = get_chat_response(prompt, st.session_state["memory"],
+                                     openai_api_key)
+    msg = {"role": "ai", "content": response}
+    st.session_state["messages"].append(msg)
+    st.chat_message("ai").write(response)
