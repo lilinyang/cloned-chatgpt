7272bef8bf7316995cd6bf4f73887c286429209b
lilinyang <lilinyang@users.noreply.github.com>
2024-08-09 11:05:38 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,8 +5,8 @@ from langchain_openai import ChatOpenAI
 
 
 def get_chat_response(prompt, memory, openai_api_key):
-   # model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key)
-    model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key, base_url="https://api.aigc369.com/v1")
+    model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key)
+   # model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key, base_url="https://api.aigc369.com/v1")
     chain = ConversationChain(llm=model, memory=memory)
 
     response = chain.invoke({"input": prompt})
