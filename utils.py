6f1d2550fe8ea59b998f95572107d0d57e096068
lilinyang <lilinyang@users.noreply.github.com>
2024-08-10 16:59:20 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,17 @@
+from langchain.chains import ConversationChain
+from langchain_openai import ChatOpenAI
+
+def get_chat_response(prompt, memory, openai_api_key):
+   # model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key)
+    model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key, base_url="https://api.aigc369.com/v1")
+    chain = ConversationChain(llm=model, memory=memory)
+
+    response = chain.invoke({"input": prompt})
+    return response["response"]
+
+
+# import os
+# from langchain.memory import ConversationBufferMemory
+# memory = ConversationBufferMemory(return_messages=True)
+# print(get_chat_response("牛顿提出过哪些知名的定律？", memory, os.getenv("OPENAI_API_KEY")))
+# print(get_chat_response("我上一个问题是什么？", memory, os.getenv("OPENAI_API_KEY")))
