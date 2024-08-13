d872032a9a07da550f5560ba8208a7729f5d773c
lilinyang <lilinyang@users.noreply.github.com>
2024-08-13 15:16:07 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,9 +9,3 @@ def get_chat_response(prompt, memory, openai_api_key):
     response = chain.invoke({"input": prompt})
     return response["response"]
 
-
-# import os
-# from langchain.memory import ConversationBufferMemory
-# memory = ConversationBufferMemory(return_messages=True)
-# print(get_chat_response("牛顿提出过哪些知名的定律？", memory, os.getenv("OPENAI_API_KEY")))
-# print(get_chat_response("我上一个问题是什么？", memory, os.getenv("OPENAI_API_KEY")))
